d474555d7e7e1c56a3c9273692f51238058211f5
Bartosz Lorek <bery.lorek@gmail.com>
2018-06-18 16:21:55 +0200

    handle keyboard states

---
+++ src/models/Keyboard.js
@@ -0,0 +1,37 @@
+const PRESSED = 1
+const RELEASED = 0
+
+class Keyboard {
+    constructor(view) {
+        this.states = new Map()
+        this.events = new Map()
+        this.listenTo(view)
+    }
+
+    on(code, callback) {
+        this.events.set(code, callback)
+    }
+
+    handleEvent(event) {
+        event.preventDefault()
+        const { code } = event
+        if (!this.events.has(code)) {
+            return
+        }
+        const keyState = event.type === 'keydown' ? PRESSED : RELEASED
+        if (this.states.get(code) !== keyState) {
+            this.states.set(code, keyState)
+            this.events.get(code)(keyState)
+        }
+    }
+
+    listenTo(view = window) {
+        ['keydown', 'keyup'].forEach(type => {
+            view.addEventListener(type, event => {
+                this.handleEvent(event)
+            })
+        })
+    }
+}
+
+export default Keyboard
