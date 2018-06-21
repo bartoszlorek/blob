fa4a1c48e8e61326d08cc69b5524b73cbfc7c028
Bartosz Lorek <bery.lorek@gmail.com>
2018-06-22 00:21:45 +0200

    deeper move system

--- src/traits/Move.js
+++ src/traits/Move.js
@@ -4,11 +4,23 @@ class Move extends Trait {
     constructor() {
         super('move')
         this.dir = 0
-        this.speed = .2
+        this.acceleration = 0.75
+        this.deceleration = 0.5
+        this.dragFactor = 0.9
     }
 
     update(entity, deltaTime) {
-        entity.vel.x += this.speed * this.dir * deltaTime
+        let absX = Math.abs(entity.vel.x)
+
+        if (this.dir !== 0) {
+            entity.vel.x += this.acceleration * deltaTime * this.dir
+
+        } else if (entity.vel.x !== 0) {
+            let deceleration = Math.min(absX, this.deceleration * deltaTime)
+            entity.vel.x += entity.vel.x > 0 ? -deceleration : deceleration
+        }
+
+        entity.vel.x *= this.dragFactor
     }
 }
 
