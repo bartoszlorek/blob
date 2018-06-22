891b964221ead44a85f95fa4c9c10b606fad6a56
Bartosz Lorek <bery.lorek@gmail.com>
2018-06-22 15:24:40 +0200

    no more jumping to the infinity

--- src/traits/Jump.js
+++ src/traits/Jump.js
@@ -1,25 +1,53 @@
 import Trait from './Trait'
+import { EDGE } from '../models/Blob'
 
 class Jump extends Trait {
     constructor() {
         super('jump')
-        this.duration = 0.25
-        this.velocity = 5
+        this.velocity = 200
+        this.duration = 0.3
+        this.gracePeriod = 0.1 // jump again before landing
+
+        this.ready = 0
+        this.requestTime = 0
         this.engageTime = 0
     }
 
+    get falling() {
+        return this.ready < 0
+    }
+
     start() {
-        this.engageTime = this.duration
+        this.requestTime = this.gracePeriod
     }
 
     cancel() {
+        this.requestTime = 0
         this.engageTime = 0
     }
 
     update(entity, deltaTime) {
+        if (this.requestTime > 0) {
+            if (this.ready > 0) {
+                this.engageTime = this.duration
+                this.requestTime = 0
+            }
+            this.requestTime -= deltaTime
+        }
+
         if (this.engageTime > 0) {
             entity.vel.y = -this.velocity
             this.engageTime -= deltaTime
+        }
+
+        this.ready--
+    }
+
+    obstruct(entity, edge) {
+        if (edge.local === EDGE.BOTTOM) {
+            this.ready = 1
+        } else if (edge.local === EDGE.TOP) {
+            this.cancel()
         }
     }
 }
