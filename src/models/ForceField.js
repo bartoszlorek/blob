fe1fd991e85ec2c8c3ac1675bf073bb77b50f120
ubery <bery.lorek@gmail.com>
2018-06-24 15:19:16 +0200

    basic structure

---
+++ src/models/ForceField.js
@@ -0,0 +1,32 @@
+class ForceField {
+    constructor() {
+        this.meshes = []
+        this.bounds = {}
+    }
+
+    top(pos) {
+
+    }
+
+    right(pos) {
+
+    }
+
+    bottom(pos) {
+
+    }
+
+    left(pos) {
+
+    }
+
+    add(mesh) {
+        this.meshes.push(mesh)
+    }
+
+    update() {
+        
+    }
+}
+
+export default ForceField
