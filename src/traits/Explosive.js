97a32e73ba318c16859a5efbe6b4db856dcfbe1e
Bartosz Lorek <bery.lorek@gmail.com>
2018-06-29 12:34:37 +0200

    add explosion animation and simple game over

--- src/traits/Explosive.js
+++ src/traits/Explosive.js
@@ -1,36 +1,68 @@
 import forEach from '../.utils/forEach'
+import Entity from '../models/Entity'
+
 import Trait from './Trait'
+import Blink from './Blink'
+import Animation from './Animation'
 
 class Explosive extends Trait {
     constructor(range = 0) {
         super('explosive')
-        this.touched = false
-        this.timeout = 0.25
         this.range = range
+        this.timer = 0.25
+
+        this.ignition = -1
+        this.exploded = false
     }
 
     start() {
-        this.touched = true
+        this.ignition++
     }
 
-    update(bomb, deltaTime) {
-        if (!this.touched) {
-            return
+    update(entity, deltaTime) {
+        if (this.ignition < 0 || this.exploded) {
+            return false
         }
-        if (this.timeout < 0) {
-            const { level } = bomb.parent
-            const affected = this.affect(bomb, [
+        if (this.ignition === 0) {
+            entity.addTrait(new Blink(0.1))
+            this.ignition++
+
+        } else if (this.timer < 0) {
+            this.exploded = true
+
+            const { level } = entity.parent
+            const blast = new Entity(
+                entity.pos.x,
+                entity.pos.y,
+                entity.size
+            )
+            level.effect.append(blast)
+            blast.addTrait(new Animation())
+            blast.animation
+                .add('blast', [
+                    [0.01, () => blast.size += this.range],
+                    [0.10, () => blast.size += this.range],
+                    [0.18, () => blast.size -= this.range / 2],
+                    [0.20, () => this.destroy(blast)]
+                ])
+                .play('blast')
+
+            const affected = this.affect(entity, [
                 level.ground,
                 level.player
             ])
             affected.forEach(this.destroy)
-            this.destroy(bomb)
+            this.destroy(entity)
 
             if (level.player.head) {
                 level.player.head.physics.fields.calculate()
+            } else {
+                // todo: proper game over
+                setTimeout(() => level.gameOver(), 1000)
             }
         }
-        this.timeout -= deltaTime
+
+        this.timer -= deltaTime
     }
 
     affect(bomb, layers) {
