81437df528985b58b6d50232a80d83df8a50b4de
Bartosz Lorek <bery.lorek@gmail.com>
2018-07-14 00:05:41 +0200

    add generate method and simulation step

--- src/models/MapGenerator.js
+++ src/models/MapGenerator.js
@@ -6,7 +6,29 @@ class MapGenerator {
     constructor(global, cols = 10, rows = 10) {
         this.global = global
         this.grid = new Grid(cols, rows)
+        this.birthLimit = 1
+        this.deathLimit = 2
+
         console.log(this)
+    }
+
+    generate(simulationSteps, initialPoints) {
+        const xOffset = Math.round(this.grid.cols / 2)
+        const yOffset = Math.round(this.grid.rows / 2)
+
+        this.grid.fill(0)
+        initialPoints.forEach(point => (
+            this.grid.cells[xOffset - point[0]][yOffset - point[1]] = 1
+        ))
+
+        let index = -1
+        while (++index < simulationSteps) {
+            this.doSimulationStep()
+        }
+
+        return this.grid.toArray()
+            .filter(cell => cell.data)
+            .map(cell => [xOffset - cell.x, yOffset - cell.y])
     }
 
     getInitialPoints(amount) {
@@ -28,6 +50,23 @@ class MapGenerator {
             }
         }
         return result
+    }
+
+    doSimulationStep() {
+        const nextGrid = this.grid.clone()
+
+        this.grid.forEach((cell, x, y) => {
+            const livingNeighbours = this.grid
+                .neighbours(x, y, true)
+                .reduce((sum, val) => sum + val, 0)
+
+            if (livingNeighbours >= 1) {
+                nextGrid.cells[x][y] = 1
+            }
+        })
+
+        this.grid = nextGrid
+        return this
     }
 }
 
