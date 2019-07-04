fac1595f0dbeecd76b7be3358ed45ab3b1a5c460
Bartosz Lorek <bartosz.lorek@brainly.com>
2019-07-04 08:01:10 +0200

    layer as a matrix

--- src/models/Layer.js
+++ src/models/Layer.js
@@ -1,53 +1,117 @@
-import {Container} from 'pixi.js';
+import {utils, Container} from 'pixi.js';
 import {extendBounds} from '@utils/pixijs';
-import EntityContainer from '@models/EntityContainer';
 
 class Layer {
   constructor(name = '') {
     this.name = name;
-    this.level = null;
-    this.solid = false;
+    this.children = [];
+    this.position = {};
 
-    this.entities = new EntityContainer();
+    // pixijs
     this.graphics = new Container();
     this.graphics.interactiveChildren = false;
 
-    this.interactive = true;
+    // parameters
+    this.resolution = 100;
     this.filterMargin = 10;
+
+    // object pools
+    this._closestArray = [];
   }
 
-  append(entity) {
-    if (entity.parent === this) {
-      return;
+  addChild(child) {
+    if (child.parent === this) {
+      return false;
     }
-    if (entity.parent !== null) {
-      entity.parent.remove(entity);
+    if (child.parent !== null) {
+      child.parent.remove(child);
     }
-    this.entities.add(entity);
-    this.graphics.addChild(entity.sprite);
-    entity.parent = this;
+    child.parent = this;
+
+    this.children.push(child);
+    this.graphics.addChild(child.sprite);
+    this._updatePosition(child);
   }
 
-  remove(entity) {
-    if (entity.parent === this) {
-      this.entities.remove(entity);
-      this.graphics.removeChild(entity.sprite);
-      entity.parent = null;
+  removeChild(child) {
+    if (child.parent !== this) {
+      return false;
     }
+    child.parent = null;
+    const index = this.children.indexOf(child);
+
+    if (index !== -1) {
+      utils.removeItems(this.children, index, 1);
+    }
+    this.graphics.removeChild(child.sprite);
+    this._removePosition(child);
   }
 
   update(deltaTime) {
-    this.entities.memo.clear();
-
-    if (this.interactive) {
-      this.entities.forEach(entity => {
-        entity.update(deltaTime);
-      });
-      this.updateFilters();
+    for (let i = 0, j = this.children.length; i < j; ++i) {
+      this.children[i].update(deltaTime);
     }
+    // it invokes after all changes
+    this._updateFilters();
   }
 
-  updateFilters() {
+  requestChange(child) {
+    // todo: optimization
+    this._updatePosition(child);
+  }
+
+  closest(x, y) {
+    const row1 = this._index(x - 1, y - 1);
+    const row2 = this._index(x - 1, y);
+    const row3 = this._index(x - 1, y + 1);
+
+    this._closestArray[0] = this.position[row1];
+    this._closestArray[1] = this.position[row1 + 1];
+    this._closestArray[2] = this.position[row1 + 2];
+
+    this._closestArray[3] = this.position[row2];
+    this._closestArray[4] = this.position[row2 + 1];
+    this._closestArray[5] = this.position[row2 + 2];
+
+    this._closestArray[6] = this.position[row3];
+    this._closestArray[7] = this.position[row3 + 1];
+    this._closestArray[8] = this.position[row3 + 2];
+
+    return this._closestArray;
+  }
+
+  closestInDirection(x, y, dX, dY) {
+    // todo: limit to bounds
+    let limit = 100;
+    let a = x;
+    let b = y;
+
+    while (limit--) {
+      const child = this.position[this._index(a, b)];
+
+      if (child) {
+        return child;
+      }
+      a += dX;
+      b += dY;
+    }
+
+    return null;
+  }
+
+  _index(x, y) {
+    return y * this.resolution + x;
+  }
+
+  _updatePosition(child) {
+    this.position[this._index(child.gridX, child.gridY)] = child;
+  }
+
+  _removePosition(child) {
+    this.position[this._index(child.gridX, child.gridY)] = undefined;
+  }
+
+  _updateFilters() {
     if (!this.graphics.filters) {
       return;
     }
