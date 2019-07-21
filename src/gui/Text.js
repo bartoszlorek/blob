0d0bfca84fb65f2620bec9386f5294efc4c7a6cc
Bartosz Lorek <bartosz.lorek@brainly.com>
2019-07-21 10:01:59 +0200

    use textNode

--- src/gui/Text.js
+++ src/gui/Text.js
@@ -4,16 +4,23 @@ class Text {
       throw 'Text requires name argument';
     }
 
+    this.text = '';
+    this.textNode = document.createTextNode('');
+
     this.node = document.createElement('div');
     this.node.className = `text text-${name}`;
+    this.node.appendChild(this.textNode);
   }
 
   set value(str) {
-    this.node.textContent = str;
+    if (str !== this.text) {
+      this.textNode.nodeValue = str;
+      this.text = str;
+    }
   }
 
   get value() {
-    return this.node.textContent;
+    return this.text;
   }
 }
 
