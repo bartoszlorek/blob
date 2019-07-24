4600e709526f3cf75f6cc86cd194c55db20b7e27
Bartosz Lorek <bartosz.lorek@brainly.com>
2019-07-24 14:24:04 +0200

    better accordion distribution

--- src/models/Background.js
+++ src/models/Background.js
@@ -5,62 +5,64 @@ class Background {
   constructor() {
     this.sprite = new Container();
     this.layers = [];
+    this.texHeight = 0;
   }
 
   set(texture, breakpoints = []) {
+    this.texHeight = texture.height;
+
     this.sprite.removeChildren();
-    this.layers = [0, ...breakpoints].map((from, index, points) => {
+    this.layers = [0, ...breakpoints].map((position, index, points) => {
       const tile = new TilingSprite(texture);
+      const height = (points[index + 1] || this.texHeight) - position;
+      const percent = height / this.texHeight;
+
       this.sprite.addChild(tile);
 
       return {
         tile,
-        from,
-        to: points[index + 1] || texture.height
+        height,
+        position,
+        percent
       };
     });
   }
 
   resize() {
-    const {length} = this.layers;
-
-    if (!length) {
+    if (!this.layers.length) {
       return;
     }
     const {innerWidth, innerHeight} = window;
-    const height = this.layers[length - 1].to;
-    const offset = (innerHeight - height) / (length - 1);
+    const difference = this.texHeight - innerHeight;
 
-    if (offset < 0) {
-      // accordion distribution
-      arrayForEach(this.layers, (layer, index) => {
-        const {tile, from, to} = layer;
+    let offset = 0;
+
+    if (difference > 0) {
+      // accordion layers distribution
+      arrayForEach(this.layers, layer => {
+        const {tile, position, height, percent} = layer;
 
         tile.visible = true;
         tile.width = innerWidth;
-        tile.height = to - from;
+        tile.height = height;
 
-        tile.tilePosition.y = -from;
+        tile.tilePosition.y = -position;
         tile.tileScale.y = 1;
+        tile.position.y = position + offset;
 
-        if (index > 0) {
-          tile.position.y = from + offset;
-        }
+        offset -= difference * percent;
       });
     } else {
-      // scale vertically first layer and hide other
-      arrayForEach(this.layers, (layer, index) => {
-        const {tile} = layer;
-
-        if (index > 0) {
-          tile.visible = false;
-        } else {
+      // just scale to fit first layer and hide other
+      arrayForEach(this.layers, ({tile}, index) => {
+        if (index === 0) {
           tile.visible = true;
           tile.width = innerWidth;
           tile.height = innerHeight;
-
           tile.tilePosition.y = 0;
-          tile.tileScale.y = innerHeight / height;
+          tile.tileScale.y = innerHeight / this.texHeight;
+        } else {
+          tile.visible = false;
         }
       });
     }
