c3f2c9d7e3285fdcce3374771092fac2acc21f75
Bartosz Lorek <bartosz.lorek@brainly.com>
2020-02-07 23:11:22 +0100

    remove move collide

--- src/traits/Move.js
+++ src/traits/Move.js
@@ -1,25 +1,16 @@
 // @flow strict
 
-import {baseSize} from '@app/constants';
 import {rotateVector} from '@utils/physics';
-import {lerp} from '@utils/math';
-
 import Vector from '@core/physics/Vector';
 import Trait from '@core/Trait';
 
-import type {EdgeType} from '@core/physics/constants';
 import type Body from '@core/physics/Body';
-
-const alignmentMin = 0.65;
-const alignmentMax = 0.99;
 
 // mutable data
 const m_vector = Vector.create();
 
 class Move extends Trait {
   direction: number;
-
-  snapToTiles: boolean;
   acceleration: number;
   deceleration: number;
   dragFactor: number;
@@ -32,7 +23,6 @@ class Move extends Trait {
     this.acceleration = 600;
     this.deceleration = 350;
     this.dragFactor = 0.95;
-    this.snapToTiles = false;
   }
 
   forward() {
@@ -64,47 +54,11 @@ class Move extends Trait {
       // prettier-ignore
       const dec = Math.min(Math.abs(velocity[axis]), this.deceleration * deltaTime);
       velocity[axis] += velocity[axis] > 0 ? -dec : dec;
+    } else {
       sprite.animation.play('idle');
     }
 
     velocity[axis] *= this.dragFactor;
-  }
-
-  collide(body: Body, edge: EdgeType) {
-    if (!this.snapToTiles) {
-      return;
-    }
-    if (this.direction !== 0) {
-      return;
-    }
-    const {min: position, gravity} = body;
-    const axis = gravity && Vector.isHorizontal(gravity.vector) ? 1 : 0;
-    const base = position[axis] / baseSize;
-
-    // fancy logic to calculate `alignment` [0-1]
-    // 0 - body is exactly between two tiles
-    // 1 - body is aligned with the closest tile
-    const n = Math.abs(base) % 1;
-    const alignment = (n < 0.5 ? 1 - n : n) * 2 - 1;
-
-    if (alignment >= alignmentMin) {
-      const tilePosition = Math.round(base) * baseSize;
-      const lerpPosition = lerp(position[axis], tilePosition, 0.2);
-
-      if (axis === 0) {
-        body.alignX(lerpPosition);
-      } else {
-        body.alignY(lerpPosition);
-      }
-
-      if (alignment >= alignmentMax) {
-        if (axis === 0) {
-          body.alignX(tilePosition);
-        } else {
-          body.alignY(tilePosition);
-        }
-      }
-    }
   }
 }
 
